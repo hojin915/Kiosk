801ddaf9dd22632d10662786f47e1382af55ab68
hojin <hojin915@naver.com>
2025-04-25 13:59:37 +0900

    for save, process division by enum

--- src/kiosk/Kiosk.java
+++ src/kiosk/Kiosk.java
@@ -6,30 +6,66 @@ import menucontainer.DrinksMenuContainer;
 
 import java.util.*;
 
-public class Kiosk extends Print{
+public class Kiosk extends Print {
     private List<Menu> menus = new ArrayList<>();
+
+    // 순서를 사용하려면 state.ordinal()
+    private enum State {
+        STEP_END,
+        STEP_START,
+        STEP_MENUS,
+        STEP_MENUITEMS
+    }
+
+    public State getState(int input) {
+        for (State state : State.values()) {
+            if (state.ordinal() == input) {
+                return state;
+            }
+        }
+        System.out.println(input + " process is not exist");
+        // 처음으로 돌아가기
+        return State.STEP_START;
+        // 프로그램 끝내기
+        //return State.STEP_END;
+    }
+
 
     // 메뉴 리스트를 직접 넣어서 생성
     public Kiosk(List<Menu> menus) {
         this.menus = menus;
     }
+
     // 직접 메뉴 추가할 때 사용
     public Kiosk() {
     }
+
     // 메뉴리스트에 메뉴 추가
     // Kiosk 클래스에서만 사용(예정)
     private void getMenus(Menu menu) {
         menus.add(menu);
     }
+
     // 작동하면 제일먼저 실행
     // 메뉴이름들 출력
     @Override
-     protected void printBody() {
+    protected void printBody() {
         int menusNumber = 1;
         for (Menu menu : menus) {
             System.out.printf("%-2d. %s\n", menusNumber, menu.getMenuName());
             menusNumber++;
         }
+    }
+
+    private int scan() {
+        Scanner sc = new Scanner(System.in);
+        int select = 0;
+        try {
+            select = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("숫자 입력받기");
+        }
+        return select;
     }
 
     /* 입력값(선택한 번호, 선택지 범위)
@@ -42,14 +78,22 @@ public class Kiosk extends Print{
         }
     }*/
 
-    private void selectMenus(int input){
+    // 메뉴목록 선택(버거 메뉴, 치킨 메뉴, 음료 메뉴)
+    private Menu selectMenu(int input) {
+        int boundary = menus.size();
+        return menus.get(input - 1);
+    }
+    // 메뉴목록 출력(버거 메뉴, 치킨 메뉴, 음료 메뉴)
+    private void printMenu(int input) {
         int boundary = menus.size();
         menus.get(input - 1).print();
     }
 
     public void startOrder() {
-        Scanner sc = new Scanner(System.in);
-        int input = 0;
+        State state = State.STEP_START;
+        // ordinal: state 관리, select: menu, mennuItem 선택 번호
+        int ordinal = 1;
+        int select = 0;
         // Menu 추가될 때마다 Container 만들어주고
         // 여기에서 추가
         Menu burgerMenu = new BurgerMenuContainer();
@@ -59,17 +103,29 @@ public class Kiosk extends Print{
         getMenus(chickenMenu);
         getMenus(drinksMenu);
         while (true) {
-            int position = 0;
-            print();
-            try {
-                input = sc.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("숫자 입력받기");
+            switch (getState(ordinal)) {
+                case STEP_END -> {
+                    // 처음으로 돌아가기
+                    //System.out.println("처음으로 돌아갑니다");
+                    //ordinal++;
+                    // 빠져나가기
+                    System.out.println("종료합니다");
+                    return;
+                }
+                case STEP_START -> {
+                    this.print();
+                    select = scan();
+                    if(select == 0) ordinal -= 1;
+                    else ordinal+= 1;
+                }
+                case STEP_MENUS -> {
+                    printMenu(select);
+                    select = scan();
+                    if(select == 0) ordinal -= 1;
+                    else ordinal += 1;
+
+                }
             }
-            if (input == 0) continue;
-            // if (input == 0) break;
-            selectMenus(input);
-            // 전체 메뉴 출력 -> 메뉴 선택하면 그 메뉴판 출력으로 변경해야함
         }
     }
 }
